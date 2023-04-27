6eb46939ebf266e353212f1b1db0a8d5100e2799
Color-Kat <katik7979@gmail.com>
2023-04-27 21:37:20 +0300

    go to home page without pc config data

--- src/UI/layout/Header.tsx
+++ src/UI/layout/Header.tsx
@@ -1,13 +1,18 @@
 import {memo} from "react";
 
 export const Header = memo(() => {
+    const goToHomePage = () => {
+        window.location = window.location.href.split('?')[0];
+    }
+
     return (
         <header
             className="flex sticky top-0 w-full py-4 justify-center shadow-xl z-20 bg-app-dark text-app-light shrink-0"
         >
             <div className="container relative flex items-center justify-between px-5 z-30">
                 <h2
-                    className="text-red-500 font-bold font-play text-2xl tracking-wider"
+                    className="text-red-500 font-bold font-play text-2xl tracking-wider cursor-pointer"
+                    onClick={goToHomePage}
                 >RX4D-configurator</h2>
             </div>
         </header>
